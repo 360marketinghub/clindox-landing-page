fd9cd31dbf0ec70c250f2c0453e24ea70196baef
Test Name <test@example.com>
2026-03-13 12:57:35 +0530

    Add PHP submission handler and update index.html with AJAX form

---
+++ submit.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $company = strip_tags(trim($_POST["company"]));
+    $message = strip_tags(trim($_POST["message"]));
+
+    // Set the recipient email address.
+    // Replace this with the user's actual email address.
+    $recipient = "yashrajbais@gmail.com"; 
+
+    // Set the email subject.
+    $subject = "New Demo Request from $name";
+
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Company: $company\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $email_headers = "From: $name <$email>";
+
+    // Send the email.
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo "Thank you! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
